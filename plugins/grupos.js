0a13724cdf578b3cd8b57daba77f1161dc746490
AsianBotV1 <104330520+AsianBotV1@users.noreply.github.com>
2022-05-25 16:16:39 -0500

    Update grupos.js

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -3,31 +3,31 @@ let fs = require('fs')
 let handler  = async (m, { conn, usedPrefix: _p }) => {
   
 let info = `
-*Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de The Shadow Borkers - Bot*
+*Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de Asian - Bot*
 
 *➤ Grupos oficiales del Bot:*
-*1.-* https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ
+*1.-* https://chat.whatsapp.com/
 
-*2.-* https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs
+*2.-* https://chat.whatsapp.com/
 
-*3.-* https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F
+*3.-* https://chat.whatsapp.com/
 
-*4.-* https://chat.whatsapp.com/CjexkGVr37J6GuSdDVAHzC
+*4.-* https://chat.whatsapp.com/
 
-*5.-* https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqgwlb
+*5.-* https://chat.whatsapp.com/
 
-*6.-* https://chat.whatsapp.com/GvrLSUSzVuT9P17CKfdxDa
+*6.-* https://chat.whatsapp.com/
 
-*7.-* https://chat.whatsapp.com/EpzuymKm6lG08k6J2Dwx7F
+*7.-* https://chat.whatsapp.com/
 
-*8.-* https://chat.whatsapp.com/IW12dLVoyWGHreGpX7rQIw
+*8.-* https://chat.whatsapp.com/
 
-*9.-* https://chat.whatsapp.com/Ef89aIuOLeD3Fa2sYmKwp7
+*9.-* https://chat.whatsapp.com/
 
-*10.-* https://chat.whatsapp.com/C2WYb1hiiijCI13QSUZLM9
+*10.-* https://chat.whatsapp.com/
 `.trim() 
 
-conn.sendMessage(m.chat, info, MessageType.text, { quoted: { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net', fromMe: false }, message: { "imageMessage": { "mimetype": "image/jpeg", "caption": '🔥 THE SHADOW BROKERS - BOT 🔥', "jpegThumbnail": fs.readFileSync(`./Menu2.jpg`)}}}})}
+conn.sendMessage(m.chat, info, MessageType.text, { quoted: { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net', fromMe: false }, message: { "imageMessage": { "mimetype": "image/jpeg", "caption": '🔥 ILLIAB Asian - BOT 🔥', "jpegThumbnail": fs.readFileSync(`./Menu2.jpg`)}}}})}
 
 handler.command = /^(grupos|gruposofc|gruposofc)$/i
 module.exports = handler
