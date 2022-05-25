8859e5534725367d026ebc51f27b2b24f76251e5
AsianBotV1 <104330520+AsianBotV1@users.noreply.github.com>
2022-05-25 16:31:22 -0500

    Update loli2.js

--- plugins/loli2.js
+++ plugins/loli2.js
@@ -5,7 +5,7 @@ if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '*[ ⚠️ ] Los coma
 let url = anime[Math.floor(Math.random() * anime.length)]
 await conn.sendMessage(m.chat, {
 contentText: '*TE VA A CAER LA POLICÍA 🚔️*',
-footerText: '©The Shadow Brokers - Bot',
+footerText: '©Asian - Bot',
 buttons: [
 { buttonId: '.loli2', buttonText: { displayText: 'SIGUIENTE' }, type: 1 }
 ],
